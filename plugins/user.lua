900d3a2588250829de7a1c2d9a4c5d8383842e3f
svanke <svanke@SVkali>
2023-11-07 19:49:29 +1000

    Added nvim-colorizer.lua settings for colors show up on style sheets

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,12 +9,33 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-{ 
+  { 
     "lukas-reineke/indent-blankline.nvim", 
     main = "ibl", 
     opts = {},
     config = function()
       require("ibl").setup()
     end,
-}, 
+  }, 
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+        require("colorizer").setup({
+                "css"; 
+                "scss"; 
+                "html"; 
+                "javascript";
+            }, 
+            {
+            RGB = true,
+            RRGGBB = true,
+            RRGGBBAA = true,
+            rgb_fn = true,
+            hsl_fn = true,
+            css = true,
+            css_fn = true,
+            mode = 'background',
+            },)
+    end,
+  },
 }
